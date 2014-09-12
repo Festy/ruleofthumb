13c91472380b09c7691da104c7dca5f040e6d545
Utsav Patel <utsav.sb.patel@gmail.com>
2014-09-12 00:47:24 -0400

    All the fields done, except - place, content.
    Signed-off-by: Utsav Patel <utsav.sb.patel@gmail.com>

--- ParseAFile.java
+++ ParseAFile.java
@@ -1,28 +1,78 @@
-package ir.rot.temp;
+package info_ret;
+
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-
-
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class ParseAFile {
 	String	fName,
 			fileID,
 			category,
-			title;
+			title,
+			author,
+			org;
 	File 	f;
-	BufferedReader reader;
 	
-public ParseAFile(String fName){
+	
+public ParseAFile(String fName) throws FileNotFoundException{
 	this.fName=fName;
 	f=new File(fName);
-	if(f.isFile()) 
 	setFileID();
 	setFileCategory();
 	setFileTitle();
+	setAuthorOrg();
+	
+}
+public String getDate() throws FileNotFoundException{
+	String sample;
+	String month="",date="";
+	BufferedReader reader;
+	reader = new BufferedReader(new FileReader(f));
+	try {
+	    sample=reader.readLine();
+	    while((sample.equals("\n") || sample.equals("\r") ||sample.equals("") ||sample.equals(null))){
+	    	sample=reader.readLine();
+	    }
+	    sample=reader.readLine();
+	    while((sample.equals("\n") || sample.equals("\r") ||sample.equals("") ||sample.equals(null))){
+	    	sample=reader.readLine();
+	    }
+	    if(sample.toLowerCase().contains("author"))
+	    	sample=reader.readLine();
+	    sample=sample.replaceAll("[()]","");
+		Pattern p1 =Pattern.compile(".*(jan.*|feb.*|mar.*|apr.*|may.*|jun.*|jul.*|aug.*|sep.*|oct.*|nov.*|dec.*)",Pattern.CASE_INSENSITIVE);
+		Matcher m1 =p1.matcher(sample);
+		if(m1.matches())
+		{	
+			month=m1.group(1).split(" ")[0];
+			Pattern p2= Pattern.compile((".*"+month+"\\s(\\d{1,2})(\\D*)(.*)"),Pattern.CASE_INSENSITIVE);
+			Matcher m2=p2.matcher(sample);
+			if(m2.matches()){
+				date=m2.group(1);
+				
+			}
+		}
+	    
+	} catch (IOException e) {
+	    e.printStackTrace();
+	} finally {
+	    try {
+	        reader.close();
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    }
+	}
+	
+
+//	System.out.println(date+" "+month);
+	return month+" "+date+" ";
 }
 private void setFileID(){
 	this.fileID=f.getName();
@@ -36,16 +86,18 @@ private void setFileCategory(){
 public String getFileCategory(){
 	return category;
 }
-public void setFileTitle(){
+public void setFileTitle() throws FileNotFoundException{
+	BufferedReader reader;
+	reader = new BufferedReader(new FileReader(f));
 	try {
 		String s;
-	    reader = new BufferedReader(new FileReader(f));
-	    s=reader.readLine();
+		s=reader.readLine();
 	    while(s.equals("\n") || s.equals("\r") ||s.equals("") ||s.equals(null))
 	    {
 	    	s=reader.readLine();
 	    }
 	    this.title=s;
+	    reader.close();
 
 	} catch (IOException e) {
 	    e.printStackTrace();
@@ -60,5 +112,42 @@ public void setFileTitle(){
 
 public String getFileTitle(){
 	return title;
+}
+
+public void setAuthorOrg(){
+	Pattern p =Pattern.compile("(<AUTHOR>)(.*)(</AUTHOR>)"); // TODO implement for <author>
+	try {
+		FileInputStream fi= new FileInputStream(f);
+		byte[] data = new byte[(int)(f).length()];
+	    try {
+			fi.read(data);
+			String s = new String(data, "UTF-8");
+			Matcher m=p.matcher(s);
+			if(m.find()){
+				String s2[],s3[];
+				s2=m.group(2).split(",");
+				s3=s2[0].split("By");
+				this.author=s3[s3.length-1];
+				if(s2.length>=2)
+					this.org=s2[s2.length-1];
+				
+			}
+			fi.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	} catch (FileNotFoundException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+
+}
+public String getAuthor(){
+	return author;
+}
+public String getOrg(){
+	return org;
 }
 }
--- Test.java
+++ Test.java
@@ -1,13 +1,67 @@
-package ir.rot.temp;
+package info_ret;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 
 public class Test {
-
-	public static void main(String[] args) {
-		ParseAFile p=new ParseAFile("C:/Users/Festy/Desktop/IR Slides/training/acq/0000005"); // You will have to write file path here.
-		System.out.println(p.fName);
-		System.out.println(p.getFileID());
-		System.out.println(p.getFileCategory());
-		System.out.println(p.getFileTitle());
+	static PrintWriter writer;
+	public static void main(String[] args) throws FileNotFoundException {
+		File[] files= new File("C:/Users/Festy/Desktop/IR Slides/training").listFiles();
+		writer = new  PrintWriter("C:/Users/Festy/Desktop/IR Slides/result.txt");
+		iterator(files);
+		writer.close();
 	}
-
+	public static void iterator(File[] files) throws FileNotFoundException
+	{
+		
+//		System.out.print(p.fName);
+		for (File file: files){
+			if(file.isDirectory()){
+				iterator(file.listFiles());
+			}
+			else
+			{
+//				System.out.println(file.getName());
+				ParseAFile p = new ParseAFile(file.getAbsolutePath());
+//				System.out.println(p.getDate());
+				writer.print(p.getFileID());
+				writer.print(" ");
+				writer.print(" ");
+				writer.print(p.getFileCategory());
+				writer.print(" ");
+				writer.print(" |");
+				writer.print(p.getFileTitle());
+				writer.print("| ");
+				writer.print(" ");
+//				writer.print(p.getAuthor());
+//				writer.print(" ");
+//				writer.print(" ");
+//				writer.print(p.getOrg());
+//				writer.print(" ");
+//				writer.print(" ");
+				writer.print(p.getDate());
+				writer.println();
+//				System.out.print(p.getFileID());
+//				System.out.print(" ");
+//				System.out.print(" ");
+//				System.out.print(p.getFileCategory());
+//				System.out.print(" ");
+//				System.out.print(" |");
+//				System.out.print(p.getFileTitle());
+//				System.out.print("| ");
+//				System.out.print(" ");
+//				System.out.print(p.getAuthor());
+//				System.out.print(" ");
+//				System.out.print(" ");
+//				System.out.print(p.getOrg());
+//				System.out.print(" ");
+//				System.out.print(" ");
+//				System.out.print(p.getDate());
+//				System.out.println();
+			}
+		}
+		
+	}
+	
 }
